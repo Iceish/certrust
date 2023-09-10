a744c0c9123ea82fc4b1d5a8b7ec447f520311a9
EnzoGzz <enzo@egonzalez.fr>
2023-09-10 18:26:19 +0200

    style: add flare color with blur

--- resources/views/web/dashboard/layout.blade.php
+++ resources/views/web/dashboard/layout.blade.php
@@ -3,7 +3,15 @@
 @section('body')
     @include('web.dashboard.partials.header')
     <main>
-        @yield('main')
-        @include('web.dashboard.partials.footer')
+        <div class="flare" style="right: -500px;top: 200px;">
+            <img src="/images/primary-flare.png" alt="flare">
+        </div>
+        <div class="flare" style="left: 0px;bottom: 400px; opacity: 0.3">
+            <img src="/images/primary-flare.png" alt="flare">
+        </div>
+        <div id="wrapper">
+            @yield('main')
+            @include('web.dashboard.partials.footer')
+        </div>
     </main>
 @endsection
