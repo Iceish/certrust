3959a5d0a8109f147faa39f6eaa9fecaa72800a3
EnzoGzz <enzo@egonzalez.fr>
2023-09-10 11:46:38 +0200

    build: add scss

--- resources/views/web/main.blade.php
+++ resources/views/web/main.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Certrust</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/styles/app.scss', 'resources/js/app.js'])
     </head>
     <body>
         @yield('body')
