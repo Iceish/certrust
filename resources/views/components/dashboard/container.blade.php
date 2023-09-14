ca6ce92a3376ffa8ef1ce1748b9a7676bb7625e1
EnzoGzz <enzo@egonzalez.fr>
2023-09-14 14:42:02 +0200

    feat: add generic container component

---
+++ resources/views/components/dashboard/container.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    @if(!empty($header))
+        <div class="container__header">
+            {{ $header }}
+        </div>
+    @endif
+
+
+    <div class="container__body">
+        {{ $body }}
+    </div>
+</div>
