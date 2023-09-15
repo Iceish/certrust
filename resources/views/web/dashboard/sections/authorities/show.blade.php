6cb9dca019a9ef7994d38f0108c1da80a7e47099
EnzoGzz <enzo@egonzalez.fr>
2023-09-15 20:23:08 +0200

    style(authority): update show nav layout

--- resources/views/web/dashboard/sections/authorities/show.blade.php
+++ resources/views/web/dashboard/sections/authorities/show.blade.php
@@ -10,12 +10,52 @@
 
     <x-dashboard.container>
         <x-slot:header>
-            <h2><i class="fa-solid fa-lock"></i> {{ $authority->common_name }}'s certificates</h2>
+            <h2><i class="fa-solid fa-lock"></i> {{ $authority->common_name }}</h2>
         </x-slot:header>
-        <x-slot:body>
-            <a href="#"><i class="fa-solid fa-file-lines"></i></a> <a href="#"><i class="fa-solid fa-key"></i></a> <a href="{{ route('dashboard.authorities.destroy', $authority->id) }}"><i class="fa-solid fa-trash"></i></a>
-            <a class="btn" href="{{ route('dashboard.authorities.create', ['type'=>'1', 'issuer'=> $authority->id]) }}">Create Sub-CA</a>
+        <x-slot:body class="authority-show__nav">
+            <div class="labeled-list">
+                <ul>
+                    <li>
+                        <div>Common name</div>
+                        <div>{{ $authority->common_name }}</div>
+                    </li>
+                    <li>
+                        <div>Organization</div>
+                        <div>{{ $authority->organization }}</div>
+                    </li>
+                    <li>
+                        <div>Organization unit</div>
+                        <div>{{ $authority->organization_unit }}</div>
+                    </li>
+                    <li>
+                        <div>Country name</div>
+                        <div>{{ $authority->country_name }}</div>
+                    </li>
+                    <li>
+                        <div>State or province name</div>
+                        <div>{{ $authority->state_or_province_name }}</div>
+                    </li>
+                    <li>
+                        <div>Locality name</div>
+                        <div>{{ $authority->locality_name }}</div>
+                    </li>
+                </ul>
+            </div>
+            <div class="vertical-separator"></div>
+            <div class="action-panel">
+                <a href="{{ route('dashboard.authorities.download', ['field'=> 'public_key', 'authority' => $authority]) }}" class="muted"><i class="fa-solid fa-file-lines"></i><p>Download CRT</p></a>
+                <a href="{{ route('dashboard.authorities.download', ['field'=> 'private_key', 'authority' => $authority]) }}" class="muted"><i class="fa-solid fa-key"></i><p>Download private key</p></a>
+                <a href="{{ route('dashboard.authorities.destroy', $authority->id) }}" class="danger"><i class="fa-solid fa-trash"></i><p>Delete</p></a>
+            </div>
 
+        </x-slot:body>
+    </x-dashboard.container>
+    <a href="{{ route('dashboard.authorities.create', ['type'=>'1', 'issuer'=> $authority->id]) }}" class="btn btn--primary create_authority_btn">
+        <i class="fa-regular fa-square-plus"></i> <p>Create Sub-CA</p>
+    </a>
+
+    <x-dashboard.container>
+        <x-slot:body>
             <div class="table">
                 <div class="cell header">
                     <div>common_name</div>
@@ -36,7 +76,7 @@
                         <div>{{ $certificate->state_or_province_name }}</div>
                         <div>{{ $certificate->locality_name }}</div>
                         <div>{{ $certificate->expires_on->format('d M, Y') }} <span class="muted">({{ date_diff($certificate->expires_on, new DateTime())->days }} days left)</span></div>
-                        <div><a class="btn" href="{{ route('dashboard.authorities.create', ['type' => 2, 'issuer' => $certificate->id]) }}">New cert</a></div>
+                        <div><a class="btn btn--primary" href="{{ route('dashboard.authorities.create', ['type' => 2, 'issuer' => $certificate->id]) }}">New cert</a></div>
                     </div>
                 @endforeach
             </div>
